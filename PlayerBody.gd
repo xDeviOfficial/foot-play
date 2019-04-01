4e851082bb67f994f6fd736f4ba0461af2f904e5
xDeviOfficial <xdevi96@gmail.com>
2019-04-01 14:52:30 +0100

    Add soccer ball etc

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -6,13 +6,26 @@ var velocity = Vector2()
 func get_input():
     # Detect up/down/left/right keystate and only move when pressed.
     velocity = Vector2()
+    var x = position.x
+    var y = position.y
+    print("position" + str(position));
+    
+ 
+    
     if Input.is_action_pressed('ui_right'):
+        if(x >= 200): return;
         velocity.x += 1
     if Input.is_action_pressed('ui_left'):
+        if(x <= -200):
+            return;
         velocity.x -= 1
     if Input.is_action_pressed('ui_down'):
+        if(y >= 200):
+            return;
         velocity.y += 1
     if Input.is_action_pressed('ui_up'):
+        if(y <= -200):
+            return;
         velocity.y -= 1
     velocity = velocity.normalized() * speed
 
