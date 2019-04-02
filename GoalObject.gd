fcd471238cda434cae6501a621c69ae534ba446e
xDeviOfficial <xdevi96@gmail.com>
2019-04-02 15:05:47 +0100

    Add Goal Texture

---
+++ GoalObject.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    self.connect("body_enter",self,"_on_Area2D_body_enter")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+    
+    
+func _on_Area2D_body_enter(body):
+    print(str('Body entered: ', body.get_name()))
