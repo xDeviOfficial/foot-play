9c85fa32fc4ec374ec8334da9cc59bb61fb41658
xDeviOfficial <xdevi96@gmail.com>
2019-04-02 13:35:44 +0100

    Set player movement limit

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -4,27 +4,26 @@ var speed = 700
 var velocity = Vector2()
 
 func get_input():
-    # Detect up/down/left/right keystate and only move when pressed.
     velocity = Vector2()
-    var x = position.x
-    var y = position.y
-    print("position" + str(position));
     
- 
+    var screen_size = get_viewport().get_visible_rect().size;
+    var x = global_position.x;
+    var y = global_position.y;
+    var padding = 35;
     
     if Input.is_action_pressed('ui_right'):
-        if(x >= 200): return;
+        if(x >= screen_size.x - padding): return;
         velocity.x += 1
     if Input.is_action_pressed('ui_left'):
-        if(x <= -200):
+        if(x <= padding):
             return;
         velocity.x -= 1
     if Input.is_action_pressed('ui_down'):
-        if(y >= 200):
+        if(y >= screen_size.y - padding):
             return;
         velocity.y += 1
     if Input.is_action_pressed('ui_up'):
-        if(y <= -200):
+        if(y <= padding):
             return;
         velocity.y -= 1
     velocity = velocity.normalized() * speed
